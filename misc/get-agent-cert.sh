40487e6ae468b1893578b2ff3ebc2f06a5b29459
jckhoe <ongjackhoe@gmail.com>
2026-01-28 12:52:31 +0800

    Update the test scripts

---
+++ misc/get-agent-cert.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -X GET http://localhost:8080/agents/agent-1/certificate -o agent-1-certs.zip
--- misc/provision-agent-cert.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl -X POST http://localhost:8080/cert/agent -H "Content-Type: application/json" -d '{"agent_id": "agent-1"}' --output agent-1-certs.zip
